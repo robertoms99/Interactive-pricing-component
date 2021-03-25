94093dcdfc8b76da4b6f2439c4c3f2dc070a9d6e
Robert Molina <rmolinas1999@gmail.com>
2021-03-24 22:39:09 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -1,47 +1,81 @@
+const priceByViews = new Map()
+priceByViews.set(10000, 8)
+priceByViews.set(50000, 12)
+priceByViews.set(100000, 16)
+priceByViews.set(500000, 24)
+priceByViews.set(1000000, 36)
+
+const totalValues = priceByViews.size
+// const steps = 100 / (- 1)
+const slideSteps = [...priceByViews.keys()].reduce((slideSteps, key, index) => {
+  slideSteps[index + 1] = key
+  return slideSteps
+}, {})
+
 const init = () => {
   let isSliderThumbFocused = false
   const slider = document.querySelector('[data-slide]')
+  const priceIndicator = document.querySelector('.js-price')
   const sliderInlineStyles = slider.style
+  const sliderThumb = slider.querySelector('[data-slide-thumb]')
   const toggle = document.querySelector('[data-toggle]')
 
-  document.addEventListener('mousedown', mouseEvent => {
-    const eventTarget = mouseEvent.target
-    const sliderThumbTarget = eventTarget.closest('[data-slide-thumb]')
-    if (sliderThumbTarget) {
-      isSliderThumbFocused = true
-    }
-  })
-
-  document.addEventListener('mouseup', () => {
+  const unFocusSliderThumb = () => {
     isSliderThumbFocused = false
-  })
-
-  document.ondragstart = function () {
-    return false
   }
 
-  const updatePercentage = mouseEvent => {
+  const focusSliderThumb = pointerEvent => {
+    isSliderThumbFocused = true
+  }
+
+  const sliderMovementHandler = pointerEvent => {
+    if (isSliderThumbFocused || pointerEvent.type === 'click') {
+      updatePercentage(
+        pointerEvent.type.includes('touch')
+          ? pointerEvent.touches[0].clientX
+          : pointerEvent.clientX
+      )
+    }
+  }
+
+  const setSlideValue = value => {
+    slider.setAttribute('data-slide-value', value)
+    setPriceByView(value)
+  }
+
+  const updatePercentage = x => {
     const { left, width } = slider.getBoundingClientRect()
-    const { clientX } = mouseEvent
-    const percentage = ((clientX - left) * 100) / width
-    sliderInlineStyles.setProperty(
-      '--calculated-percentage',
-      Math.max(0, Math.min(percentage, 100))
+    const percentage = Math.max(0, Math.min(((x - left) * 100) / width, 100))
+    const stepValue = Math.round(
+      Math.max(1, Math.min(totalValues, (percentage * totalValues) / 100))
     )
+    sliderInlineStyles.setProperty('--calculated-percentage', percentage)
+    setSlideValue(slideSteps[stepValue])
   }
+
+  const getSlideViewsValue = () =>
+    Number.parseInt(slider.getAttribute('data-slide-value'))
+
+  const validIsBillingIsActive = () =>
+    toggle.hasAttribute('data-toggle-checked')
 
   const handleToggle = () => {
     toggle.toggleAttribute('data-toggle-checked')
+    setPriceByView(getSlideViewsValue())
   }
 
-  slider.addEventListener('click', updatePercentage)
-  toggle.addEventListener('click', handleToggle)
+  const setPriceByView = views => {
+    let price = priceByViews.get(views)
+    price = validIsBillingIsActive() ? price * 0.75 : price
+    priceIndicator.textContent = `$${price.toFixed(2)}`
+  }
 
-  document.addEventListener('mousemove', mouseEvent => {
-    if (isSliderThumbFocused) {
-      updatePercentage(mouseEvent)
-    }
-  })
+  sliderThumb.addEventListener('pointerdown', focusSliderThumb)
+  document.onmouseup = document.ontouchcancel = document.onmouseleave = document.ontouchend = unFocusSliderThumb
+  document.onmousemove = document.ontouchmove = slider.onclick = sliderMovementHandler
+  document.ondragstart = () => false
+  setPriceByView(getSlideViewsValue())
+  toggle.addEventListener('click', handleToggle)
 }
 
 document.addEventListener('DOMContentLoaded', init)
